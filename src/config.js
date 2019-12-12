537d09289f949fba53938f6ae440186b80a6178f
gagsdeep <gagandeep.wadhwa@gmail.com>
2019-12-12 13:39:32 +0530

    Update config.js
    Updated config file

--- src/config.js
+++ src/config.js
@@ -1,5 +1,6 @@
 export default {
 
     URL_ROOT : 'http://api.openweathermap.org/data/2.5/',
-    API_KEY : '1435d501cc050aefc9e85f8ffe80d9a2'
+    API_KEY : '1435d501cc050aefc9e85f8ffe80d9a2',
+    API_URL:'http://www.example.com/
 }
